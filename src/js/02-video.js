7c5b017158b409ab56860e800e2994e29295f0a9
Anna Samsonenko <foster.anna93@gmail.com>
2021-10-25 13:08:57 +0300

    add solved task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('play', onPlay);
+
+function onPlay() {
+  console.log('played the video!');
+  player.on('timeupdate', throttle(setCurrentTimeToStorage, 1000));
+}
+
+function setCurrentTimeToStorage(event) {
+  console.log(event);
+  localStorage.setItem(STORAGE_KEY, event.seconds);
+}
+
+function getCurrentTimeFromStorage() {
+  return localStorage.getItem(STORAGE_KEY);
+}
+
+player.setCurrentTime(getCurrentTimeFromStorage());
