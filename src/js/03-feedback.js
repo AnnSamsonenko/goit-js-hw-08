8f830251808b06cb9ed5244d182c670b74c86ae7
Anna Samsonenko <foster.anna93@gmail.com>
2021-11-06 17:51:31 +0200

    fixed task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,35 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+initForm();
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  input: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
-
-const formData = {};
-getSavedFormData();
-
-refs.textarea.addEventListener('input', throttle(onInput, 500));
-refs.input.addEventListener('input', throttle(onInput, 500));
-refs.form.addEventListener('submit', onFormSubmit);
-
-function onInput(event) {
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-function getSavedFormData() {
-  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedFormData) {
-    refs.textarea.value = savedFormData.message;
-    formData.message = savedFormData.message;
-    refs.input.value = savedFormData.email;
-    formData.email = savedFormData.email;
-  }
-}
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('change', throttle(onInputChange, 500));
+form.addEventListener('reset', () => {
+  localStorage.removeItem(STORAGE_KEY);
+});
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(formData);
-  event.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  const formData = new FormData(this);
+  formData.forEach((value, name) => console.log(value, name));
+  this.reset();
+}
+
+function onInputChange(event) {
+  let savedFormData = localStorage.getItem(STORAGE_KEY);
+  savedFormData = savedFormData ? JSON.parse(savedFormData) : {};
+  savedFormData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedFormData));
+}
+
+function initForm() {
+  let savedFormData = localStorage.getItem(STORAGE_KEY);
+  if (savedFormData) {
+    savedFormData = JSON.parse(savedFormData);
+    Object.entries(savedFormData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
 }
